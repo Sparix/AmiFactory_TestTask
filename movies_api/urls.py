b11a19644008cbdd3f83f74040f8e64a422e338c
Olexandr Hontarenko <sanya221138@gmail.com>
2024-04-12 14:54:10 +0300

    create MovieListView api, and create endpoint for this view

--- movies_api/urls.py
+++ movies_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from movies_api.views import GenreListView
+from movies_api.views import GenreListView, MovieListView
 
 urlpatterns = [
     path("genres/", GenreListView.as_view(), name="genres"),
+    path("movies/", MovieListView.as_view(), name="movies")
 ]
 
 app_name = "movies_api"
--- movies_api/views.py
+++ movies_api/views.py
@@ -1,6 +1,7 @@
+from django.core.paginator import Paginator, EmptyPage
 from django.http import JsonResponse
 from django.views.generic import ListView
-from movies_api.models import Genre
+from movies_api.models import Movie, Genre
 
 
 class GenreListView(ListView):
@@ -10,3 +11,62 @@ class GenreListView(ListView):
     def get(self, request, *args, **kwargs):
         data = list(self.get_queryset().values("id", "title"))
         return JsonResponse(data, safe=False)
+
+
+class MovieListView(ListView):
+    model = Movie
+    paginate_by = 5
+    queryset = Movie.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        page_number = self.request.GET.get("page", 1)
+        paginator = Paginator(queryset, self.paginate_by)
+
+        try:
+            page_obj = paginator.page(page_number)
+        except EmptyPage:
+            return JsonResponse({"errors": ["page__out_of_bounds"]})
+
+        data = self.get_paginated_data(page_obj)
+        return JsonResponse(data)
+
+    def get_paginated_data(self, page_obj):
+        serialized_movies = [self.serialize_movie(movie) for movie in page_obj]
+        return {
+            "total": page_obj.paginator.count,
+            "pages": page_obj.paginator.num_pages,
+            "results": serialized_movies,
+        }
+
+    def serialize_movie(self, movie):
+        return {
+            "id": movie.id,
+            "title": movie.title,
+            "description": movie.description,
+            "release_year": movie.release_year,
+            "mpa_rating": movie.mpa_rating,
+            "imdb_rating": float(movie.imdb_rating),
+            "duration": movie.duration,
+            "poster": movie.poster.url if movie.poster else "",
+            "bg_picture": movie.bg_picture.url if movie.bg_picture else "",
+            "genres": self.serialize_genres(movie.genres.all()),
+            "directors": self.serialize_people(movie.directors.all()),
+            "writers": self.serialize_people(movie.writers.all()),
+            "stars": self.serialize_people(movie.stars.all()),
+        }
+
+    @staticmethod
+    def serialize_genres(genres):
+        return [{"id": genre.id, "title": genre.title} for genre in genres]
+
+    @staticmethod
+    def serialize_people(people):
+        return [
+            {
+                "id": person.id,
+                "first_name": person.first_name,
+                "last_name": person.last_name,
+            }
+            for person in people
+        ]
