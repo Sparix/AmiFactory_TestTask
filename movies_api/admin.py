42de45fe80ff8c7cc1e500395ba9f3c5d44a227a
Olexandr Hontarenko <sanya221138@gmail.com>
2024-04-12 12:40:34 +0300

    gitignore add images, and add models to admin panel

--- movies_api/admin.py
+++ movies_api/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from movies_api.models import Genre, Person, Movie
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_filter = ("title",)
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    search_fields = ("last_name", "types")
+    list_filter = ("types",)
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "release_year",
+        "mpa_rating",
+        "imdb_rating",
+        "duration",
+        "created_at",
+    )
+    list_filter = ("imdb_rating", "mpa_rating",)
