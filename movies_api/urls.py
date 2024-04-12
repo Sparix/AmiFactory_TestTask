51e068db88b24acc4133abd7284ae0cb45d47f37
Olexandr Hontarenko <sanya221138@gmail.com>
2024-04-12 13:49:47 +0300

    add views for Genre models, and endpoint for this view

--- movies_api/urls.py
+++ movies_api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+from movies_api.views import GenreListView
+
 urlpatterns = [
+    path("genres/", GenreListView.as_view(), name="genres"),
 ]
 
 app_name = "movies_api"
--- movies_api/views.py
+++ movies_api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.generic import ListView
+from movies_api.models import Genre
 
-# Create your views here.
+
+class GenreListView(ListView):
+    model = Genre
+    queryset = Genre.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        data = list(self.get_queryset().values("id", "title"))
+        return JsonResponse(data, safe=False)
